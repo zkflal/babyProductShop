e3997c4dd27b00a161073022c9132e6c249a8ae8
[SW7] 곽동선 <sunstar8022@gmail.com>
2023-11-03 18:03:27 +0900

    feat : orderModel - UserId 추가

--- BabyShop/src/orders/models/orderModel.js
+++ BabyShop/src/orders/models/orderModel.js
@@ -11,6 +11,8 @@ const ProductInfosType = new mongoose.Schema({
 
 const orderSchema = new mongoose.Schema({
     shortId,    // OrderId
+
+    UserId: {type: String, required: true},
     Name: {type: String, required: true},
     Address: {type: String, required: true},
     Phone: {type: String, required: true},
