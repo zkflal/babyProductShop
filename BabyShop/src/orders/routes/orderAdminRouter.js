70d196400054de7c6d4e18233af4e684a1c36b82
[SW7] 곽동선 <sunstar8022@gmail.com>
2023-11-06 10:58:25 +0900

    fixed untracked files

--- BabyShop/src/orders/routes/orderAdminRouter.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const {adminUpdateOrder, adminDeleteOrder} = require("../services/orderService");
-
-router.put('/', adminUpdateOrder);
-router.delete('/:id', adminDeleteOrder);
-
-module.exports = router;
--- BabyShop/utils/hashedPwd.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-module.exports = (password) => {
-    const hash = crypto.createHash('sha1')
-                        .update(password)
-                        .digest('hex');
-        return hash;
-} 
